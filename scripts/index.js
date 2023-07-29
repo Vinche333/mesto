00246dd0638c3d4a2aa1c88021a9753e0861c1c6
Ivan Venediktov <131746772+Vinche333@users.noreply.github.com>
2023-07-29 19:20:57 +0700

    reject

--- scripts/index.js
+++ scripts/index.js
@@ -20,13 +20,6 @@ export const profileElements = {
   job: profileJob,
 };
 
-
-export const profileEditButton = document.querySelector('.profile__button_type_edit');
-export const profileEditPopupSelector = '.popup_type_edit-profile';
-const profileEditPopupElement = document.querySelector(profileEditPopupSelector);
-export const profileNameInput = profileEditPopupElement.querySelector('.popup__input_type_name');
-export const profileJobInput = profileEditPopupElement.querySelector('.popup__input_type_job');
-
 function handleOpenForm() {
     formValidators[this.formName].hideErrors();
     formValidators[this.formName].disableButtonState();
@@ -35,27 +28,103 @@ function handleOpenForm() {
   const profileEditPopup = new PopupWithForm(profileEditPopupSelector, data => {
     profileEditPopup.blockSubmitButton();
   
-    api.setUserInfo(data)
-      .then(res => {
-        userInfo.fill(res);
-        userInfo.renderName();
-        userInfo.renderJob();
-        profileEditPopup.close();
+const config = {
+  popupForm : '.popup__form',
+  inputErrorClass : 'form__input_type_error',
+  inputErrorActive : 'form__input-error_active',
+  formInput : '.form__input',
+  formSubmit : '.form__submit',
+};
+
+export {popupEditProfile, profileEditBtn, formEditProfile, nameInput, jobInput, formAddNewCard,
+  popupAddNewCardOpenBtn, popupAddNewCard, config, buttonEditAvatar, formEditAvatar,
+avatar};
+
+function fillInEditProfileFormInputs({ username, job }) {
+  nameInput.value = username;
+  jobInput.value = job;
+}
+
+const editAvatarPopup = new PopupWithForm({
+  popupSelector: '.popup_type_avatar',
+  handleFormSubmit: (data) => {
+    editAvatarPopup.loading(true);
+    api.editAvatar(data)
+      .then((data) => {
+        avatar.src = data.avatar;
+        editAvatarPopup.close();
       })
-      .catch(err => console.error(err))
+      .catch((err) => {
+        console.log(`Ошибка: ${err}`);
+      })
       .finally(() => {
-        profileEditPopup.unblockSubmitButton();
+        editAvatarPopup.loading(false);
       });
-  }, handleOpenForm);
-
-  profileEditPopup.setEventListeners();
-
-profileEditButton.addEventListener('click', function () {
-  ({
-    name: profileNameInput.value,
-    job: profileJobInput.value
-  } = userInfo.getUserInfo());
-  profileEditPopup.open();
-  profileNameInput.dispatchEvent(new Event('input'));
-  profileJobInput.dispatchEvent(new Event('input'));
+  }
 });
+editAvatarPopup.setEventListeners();
+buttonEditAvatar.addEventListener('click', () => {
+  formEditAvatarValidator.toggleButtonState();
+  editAvatarPopup.open();
+});
+profileEditBtn.addEventListener('click', () => {
+  const info = userInfo.getUserInfo();
+  fillInEditProfileFormInputs({
+    username: info.username,
+    job: info.job
+  });
+  editProfilePopup.open();
+});
+
+  open() {
+    this._popup.classList.add('popup_opened');
+    document.addEventListener('keydown', this._escapeClose);
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._escapeClose);
+  }
+
+  setEventListeners() {
+    this._closeButton.addEventListener('click', () => {
+      this.close();
+    });
+    this._popup.addEventListener('mousedown', (event) => {
+      if (event.target.classList.contains('popup')) {
+        this.close();
+      }
+    });
+  }
+}
+
+export default class UserInfo {
+  constructor({ username, job,}) {
+    this._username = document.querySelector(username);
+    this._job = document.querySelector(job);
+  }
+
+  getUserInfo() {
+    const userInfo = {
+      username: this._username.textContent,
+      job: this._job.textContent,
+    }
+
+    return userInfo;
+  }
+
+  setUserInfo(data) {
+    this._username.textContent = data.name;
+    this._job.textContent = data.about;
+  }
+}
+
+export default class PopupWithForm extends Popup {
+  constructor({ popupSelector, handleFormSubmit }) {
+    super(popupSelector);
+    this._handleFormSubmit = handleFormSubmit;
+    this._popupForm = this._popup.querySelector('.popup__form');
+    this._inputList = this._popupForm.querySelectorAll('.form__input');
+    this._submitBtn = this._popupForm.querySelector('.form__submit');
+    this._submitBtnText = this._submitBtn.textContent;
+  }
